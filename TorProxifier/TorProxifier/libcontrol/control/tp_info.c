decde31b0e6f07d32452a2c16a3cab75cf21a3f9
Julien-Pierre Avérous <git@sourcemac.com>
2016-02-26 23:00:08 +0100

    - Update header licence.

--- TorProxifier/TorProxifier/libcontrol/control/tp_info.c
+++ TorProxifier/TorProxifier/libcontrol/control/tp_info.c
@@ -1,10 +1,24 @@
-//
-//  mach_helper.c
-//  control
-//
-//  Created by Julien-Pierre Avérous on 26/02/2016.
-//  Copyright © 2016 Julien-Pierre Avérous. All rights reserved.
-//
+/*
+ *  tp_info.c
+ *
+ *  Copyright 2016 Avérous Julien-Pierre
+ *
+ *  This file is part of TorProxifier.
+ *
+ *  TorProxifier is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  TorProxifier is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with TorProxifier.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
 #include <string.h>
 #include <stdlib.h>
@@ -24,6 +38,7 @@
 
 #define SMRoundUp(Value, Round)		(((Value) + ((Round) - 1)) & ~((Round) - 1))
 #define SMRoundDown(Value, Round)	((Value) & ~((Round) - 1))
+
 
 
 /*
--- TorProxifier/TorProxifier/libcontrol/control/tp_info.h
+++ TorProxifier/TorProxifier/libcontrol/control/tp_info.h
@@ -1,10 +1,24 @@
-//
-//  mach_helper.h
-//  control
-//
-//  Created by Julien-Pierre Avérous on 26/02/2016.
-//  Copyright © 2016 Julien-Pierre Avérous. All rights reserved.
-//
+/*
+ *  tp_info.h
+ *
+ *  Copyright 2016 Avérous Julien-Pierre
+ *
+ *  This file is part of TorProxifier.
+ *
+ *  TorProxifier is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  TorProxifier is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with TorProxifier.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
 #pragma mark once
 
